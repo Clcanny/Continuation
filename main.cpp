37c89bb6239362870a0eb8eb42956c636e38c0fc
demons <837940593@qq.com>
2017-11-12 08:36:25 +0000

    meet a bug

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 int main()
 {
-    Continuation source = callcc(
+    Continuation source_a = callcc(
 	[](Continuation &&sink) {
     	    std::cout << "I'm in continuation" << std::endl;
     	    sink = sink.resume();
@@ -12,5 +12,14 @@ int main()
     	}
     );
 
-    source = source.resume();
+    Continuation source_b = callcc(
+	[](Continuation &&sink) {
+	    sink = sink.resume();
+	    std::cout << "I'm in another continuation" << std::endl;
+	    return std::move(sink);
+	}
+    );
+
+    source_a = source_a.resume();
+    source_b = source_b.resume();
 }
