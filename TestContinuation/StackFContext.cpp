b4e190d91fa2a32ed3a5d9909d5b1ee130c21f7a
demons <837940593@qq.com>
2017-11-21 13:51:28 +0000

    .

--- TestContinuation/StackFContext.cpp
+++ TestContinuation/StackFContext.cpp
@@ -1,10 +1,62 @@
 #include "fcontext.h"
+#include <cstdlib>
+#include <stdexcept>
+#include <cassert>
+#include <iostream>
 
 class SimpleStackAllocator
+{
+    public:
+	static std::size_t const size = 128;
+
+	void *allocate() const
+	{
+	    void *sp = std::malloc(size);
+	    if (!sp)
+	    {
+		throw std::bad_alloc();
+	    }
+	    return static_cast<char*>(sp) + size;
+	}
+
+	void deallocate(void *sp, std::size_t size) const
+	{
+	    assert(sp != nullptr);
+	    void *p = static_cast<char *>(sp) - size;
+	    std::free(p);
+	}
+};
+
+int value;
+
+void f1(transfer_t t)
+{
+    value = 3;
+    jump_fcontext( t.fctx, 0);
+}
+
+void f2(transfer_t t)
+{
+    std::cout << "f1: entered" << std::endl;
+    SimpleStackAllocator alloc;
+    void * sp = alloc.allocate();
+    fcontext_t ctx = make_fcontext(sp, SimpleStackAllocator::size, f1);
+    jump_fcontext(ctx, 0);
+    jump_fcontext(t.fctx, 0);
+}
 
 void stacked()
 {
-    int value1 = 0;
-    StackAllocator sa;
-    void *sp = sa.allocate();
+    value = 0;
+    SimpleStackAllocator alloc;
+    void *sp = alloc.allocate();
+    fcontext_t ctx = make_fcontext(sp, SimpleStackAllocator::size, f2);
+    jump_fcontext(ctx, 0);
+    assert(value == 3);
+    alloc.deallocate(sp, SimpleStackAllocator::size);
+}
+
+int main()
+{
+    stacked();
 }
--- main.cpp
+++ main.cpp
@@ -1,28 +1,62 @@
-#include "continuation_fcontext.h"
+#include "fcontext.h"
+#include <cstdlib>
+#include <stdexcept>
+#include <cassert>
 #include <iostream>
+
+class SimpleStackAllocator
+{
+    public:
+	static std::size_t const size = 128;
+
+	void *allocate() const
+	{
+	    void *sp = std::malloc(size);
+	    if (!sp)
+	    {
+		throw std::bad_alloc();
+	    }
+	    return static_cast<char*>(sp) + size;
+	}
+
+	void deallocate(void *sp, std::size_t size) const
+	{
+	    assert(sp != nullptr);
+	    void *p = static_cast<char *>(sp) - size;
+	    std::free(p);
+	}
+};
+
+int value;
+
+void f1(transfer_t t)
+{
+    value = 3;
+    jump_fcontext( t.fctx, 0);
+}
+
+void f2(transfer_t t)
+{
+    std::cout << "f1: entered" << std::endl;
+    SimpleStackAllocator alloc;
+    void * sp = alloc.allocate();
+    fcontext_t ctx = make_fcontext(sp, SimpleStackAllocator::size, f1);
+    jump_fcontext(ctx, 0);
+    jump_fcontext(t.fctx, 0);
+}
+
+void stacked()
+{
+    value = 0;
+    SimpleStackAllocator alloc;
+    void *sp = alloc.allocate();
+    fcontext_t ctx = make_fcontext(sp, SimpleStackAllocator::size, f2);
+    jump_fcontext(ctx, 0);
+    assert(value == 3);
+    alloc.deallocate(sp, SimpleStackAllocator::size);
+}
 
 int main()
 {
-    Continuation source_a = callcc(
-	[](Continuation &&sink) {
-    	    std::cout << "I'm in continuation" << std::endl;
-    	    sink = sink.resume();
-    	    std::cout << "I'm in continuation" << std::endl;
-    	    return std::move(sink);
-    	}
-    );
-
-    Continuation source_b = callcc(
-	[](Continuation &&sink) {
-	    sink = sink.resume();
-	    std::cout << "I'm in another continuation" << std::endl;
-	    sink = sink.resume();
-	    std::cout << "I'm in another continuation" << std::endl;
-	    return std::move(sink);
-	}
-    );
-
-    source_a = source_a.resume();
-    source_b = source_b.resume();
-    source_b = source_b.resume();
+    stacked();
 }
